99d12db9c36b6d937e9e54943c2c160883bf220f
Dhanesh Purohit <dhanesh.purohit@gmail.com>
2011-07-25 02:36:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YogaS::Application.config.secret_token = '7ca613a795f2bb04f80baf23b8ad16c88992e5bd7bd5d23963282ca90603880cdff9383728b2f113ce22b755b38141b49e1f272fd971a7cb257f011da57d1dbc'
